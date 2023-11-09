251bcd4b26d01c316002e1790788ad2ff0b60690
RinneT3nsei <58113647+RinneT3nsei@users.noreply.github.com>
2023-11-09 16:30:16 +0200

    Додав файли по лабораторній №5

---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using(Aplication db = new Aplication()) 
+            {
+
+                Book book1 = new Book
+                {
+                    Id = 1,
+                    Title = "The Great Gatsby",
+                    Description = "A novel by F. Scott Fitzgerald",
+                    Author = "F. Scott Fitzgerald"
+                };
+                Book book2 = new Book
+                {
+                    Id = 2,
+                    Title = "To Kill a Mockingbird",
+                    Description = "A novel by Harper Lee",
+                    Author = "Harper Lee"
+                };
+                Book book3 = new Book
+                {
+                    Id = 3,
+                    Title = "1984",
+                    Description = "A novel by George Orwell",
+                    Author = "George Orwell"
+                };
+                db.Books.AddRange(book1, book2, book3);
+                db.SaveChanges();
+            }
+            // получение данных
+            using (Aplication db = new Aplication()) 
+            {
+                var _books = db.Books.ToList();
+                Console.WriteLine("Список Об'єктiв");
+                foreach (Book u in _books)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Title}.{u.Description}.{u.Author}");
+                }
+            }
+        }
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Author { get; set; }
+        }
+        public class Aplication : DbContext
+        {
+            public DbSet<Book> Books { get; set; } = null;
+            public Aplication() 
+            {
+                Database.EnsureCreated();
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseMySql("server=localhost;user=root;password=3514;database=librarydb;",
+                    new MySqlServerVersion(new Version(8, 0, 35)));
+            }
+        }
+    }
+}
---
+++ Lab_5_3/Lab_5_3/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (Aplication db = new Aplication())
+            {
+
+                // створення об'єктів класу Task
+                Task task1 = new Task
+                {
+                    Id = 1,
+                    Title = "Вiдправити лист",
+                    Description = "Написати i вiдправити лист клiєнту",
+                    Status = TaskStatus.InProcess
+                };
+                Task task2 = new Task
+                {
+                    Id = 2,
+                    Title = "Зробити презентацiю",
+                    Description = "Пiдготувати i провести презентацiю для клiєнта",
+                    Status = TaskStatus.Completed
+                };
+                Task task3 = new Task
+                {
+                    Id = 3,
+                    Title = "Написати статтю",
+                    Description = "Написати статтю для блогу",
+                    Status = TaskStatus.Rejected
+                };
+                // збереження об'єктів у базу даних
+                db.Tasks.AddRange(task1, task2, task3);
+                db.SaveChanges();
+            }
+            // отримання даних з бази даних
+            using (Aplication db = new Aplication())
+            {
+                var _tasks = db.Tasks.ToList();
+                Console.WriteLine("Список завдань");
+                foreach (Task u in _tasks)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Title}.{u.Description}.{u.Status}");
+                }
+            }
+        }
+
+        public enum TaskStatus
+        {
+            InProcess,
+            Completed,
+            Rejected
+        }
+
+        public class Task
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public TaskStatus Status { get; set; }
+        }
+        public class Aplication : DbContext
+        {
+            public DbSet<Task> Tasks { get; set; } = null;
+            public Aplication()
+            {
+                Database.EnsureCreated();
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseMySql("server=localhost;user=root;password=3514;database=tasksdb;",
+                    new MySqlServerVersion(new Version(8, 0, 35)));
+            }
+        }
+    }
+}
