f51b690b3746d1ba246d0db648aa21b6d70f86ed
RinneT3nsei <58113647+RinneT3nsei@users.noreply.github.com>
2023-11-10 16:53:01 +0200

    Додав файли по лабораторній №6

---
+++ WcfServiceLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(WcfServiceLibrary1.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Хост запустився");
+                Console.ReadLine();
+            }
+        }
+    }
+}
